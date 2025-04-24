32ea01ef0b753f680d37039fc3d9cccfe6d8eef5
sherly-23 <patilanku111@gmail.com>
2025-04-24 11:09:45 +0530

    save file

---
+++ demo1.java
@@ -0,0 +1,8 @@
+public class demo1{
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        System.out.println("Addition:"+(a+b));
+        System.out.println("Substraction:"+(a-b));
+    }
+}
